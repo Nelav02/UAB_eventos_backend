66b3bf6544d3114f5bc10e041c6dac3e2487dbfb
Nelav <valenntino705@gmail.com>
2023-11-01 20:55:49 -0400

    Iniciando el proyecto

---
+++ src/main/java/uab/eventos_backend/UabEventosBackendApplication.java
@@ -0,0 +1,13 @@
+package uab.eventos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UabEventosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UabEventosBackendApplication.class, args);
+	}
+
+}
